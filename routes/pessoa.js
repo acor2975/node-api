629f1b4d2e63ddb067847e362e9917239f2b6571
Ana Rodrigues <acor2975@gmail.com>
2019-01-28 22:08:29 -0200

    Mudança na resposta do post, put e delete de pessoa

--- routes/pessoa.js
+++ routes/pessoa.js
@@ -20,7 +20,11 @@ router.get('/:id', async function (req, res) {
 router.post('/', async function (req, res) {
     var nome = req.body.nome
     var pessoa = await PessoaController.criar({ nome: nome })
-    res.send(pessoa)
+    var response = {
+        message: 'Pessoa criada com sucesso!',
+        data: pessoa
+    }
+    res.send(response)
 })
 
 router.put('/:id', async function (req, res) {
@@ -28,7 +32,11 @@ router.put('/:id', async function (req, res) {
     var nome = req.body.nome
     try {
         var pessoa = await PessoaController.atualizar({ id: id, nome: nome })
-        res.send(pessoa)
+        var response = {
+            message: 'Pessoa atualizada com sucesso!',
+            data: pessoa
+        }
+        res.send(response)
     } catch (err) {
         res.status(400).send('ops! Pessoa nao encontrada.')
     }
@@ -39,7 +47,11 @@ router.delete('/:id', async function (req, res) {
     try {
         var pessoa = await PessoaController.remover(id)
         if (!pessoa) throw new Error()
-        res.send(pessoa)
+        var response = {
+            message: 'Pessoa removida com sucesso!',
+            data: pessoa
+        }
+        res.send(response)
     } catch (err) {
         res.status(400).send('ops! Pessoa nao encontrada.')
     }
